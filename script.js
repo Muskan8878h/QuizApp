a8172a8a710651a6f29c369819918c8be05d045d
muskan <muskan8878h@gmail.com>
2024-10-07 11:51:25 +0530

    create an array of questions inside this create questions and option

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-
+const questions= [
+    {
+        'que': 'Which of the following is a Markup Language?',
+        'a':'HTML',
+        'b':'CSS',
+        'c':'JavaScript',
+        'd':'PHp',
+        'correct':'a'
+    },
+    {
+        'que': 'Whatyear was js launched?',
+        'a':'1996',
+        'b':'1995',
+        'c':'1994',
+        'd':'none of the above',
+        'correct':'b'
+    },
+    {
+        'que': 'What does CSS stands for?',
+        'a':'Hypertext Markup language',
+        'b':'Cascading style Sheet',
+        'c':'Jason Object Notation',
+        'd':'Cascading sheet',
+        'correct':'b'
+    }
+]
